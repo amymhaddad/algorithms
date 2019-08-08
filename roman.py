d3d98eb467282d2c2b911d00dec9889e25859017
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-08 07:42:30 -0400

    Working on algorithm - WIP

---
+++ roman.py
@@ -0,0 +1,43 @@
+"""Given a roman numeral, find the integer equivalent"""
+
+roman_numerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IV", "X"]
+# integers = [num for num in range(1, 11)]
+
+# roman = "X"
+
+# def roman_to_int(roman):
+#     """Convert a roman numeral to an integer"""
+
+#     roman_to_int = dict(zip(roman_numerals, integers))
+#     for num in roman:
+#         return roman_to_int[num]
+# print(roman_to_int(roman))
+
+number = 1
+
+
+roman_numeral_values = {
+    1: "I",
+    4: "IV",
+    5: "V",
+    9: "IX",
+    10: "X",
+}
+
+conversion = []
+
+# for num in range(1, number+1):
+#     if num % 5:
+#         x = roman_numeral_values.get(num)
+
+
+multiples = []
+for nums in roman_numeral_values.keys():
+    if number % nums == 0 and nums > 1:
+        if nums == number:
+            print(roman_numeral_values.get(nums))
+        # multiples.append(nums)
+    
+    
+
+# print(multiples)
