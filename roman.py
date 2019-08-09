d4f57faa63b549dc7df6a0b16e7e1b1ce20a7031
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-09 07:07:23 -0400

    Got algorithm for second base case

--- roman.py
+++ roman.py
@@ -17,19 +17,13 @@ roman_numerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IV", "X"]
 
 roman_numeral_values = {
     1: "I",
-    4: "IV",
     5: "V",
-    9: "IX",
     10: "X",
 }
 
-number = 14
+number = 15
 
-#Write an algo that gets the mutliples for 5, 10, 4, 9. Then, cycle through given number and see if there are any multiples matches
-#Then match the multiples 
-
-
-def int_vals_to_extract(roman_numeral_values):
+def exact_match(roman_numeral_values):
     vals_to_extract = []
 
     for nums in roman_numeral_values.keys():
@@ -38,24 +32,92 @@ def int_vals_to_extract(roman_numeral_values):
         if converted_value:
             vals_to_extract.append(converted_value)
             return vals_to_extract
+    
+numbers_to_extract = exact_match(roman_numeral_values)
+
+
+multiple_five = []
+for num in range(1, number+1):
+    if num % 5 == 0:
+        multiple_five.append(num)
+
+nums_to_convert = []
+for num in range(1, number +1):
+    if num in multiple_five and num in roman_numeral_values.keys():
+        nums_to_convert.append(num)
+
+number_to_roman_numeral = []
+for num in nums_to_convert:
+    letter = roman_numeral_values.get(num)
+    number_to_roman_numeral.append(letter)
+print(number_to_roman_numeral)
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def int_vals_to_extract(roman_numeral_values):
+
+#     convert = 0
+#     counts = []
+#     for num in range(1, number+1):
+#         if num in roman_numeral_values.keys():
+#             counts.append(num)
+
+     
+# print(int_vals_to_extract(roman_numeral_values))
+
+    # #Get the highest common val that's in the dictionary and then remove it
+    # start_value = max(counts)
+    # convert = start_value
+    # counts.remove(start_value)
+
+    # #cycle through the remaining common values
+    # while convert < number:
+    #     for value in counts:
+    #         print(value)
+
         
-        else:
-            common_nums = []
-            for num in range(1, number+1):
-                if num in roman_numeral_values.keys():
-                    common_nums.append(num)
-            max_common_nums = max(common_nums)
-            difference = number - max_common_nums
-            print(difference)
+        
+
+    
+    
+ 
+
+
+      
+        # else:
+        #     common_nums = []
+        #     for num in range(1, number+1):
+        #         if num in roman_numeral_values.keys():
+        #             common_nums.append(num)
+        #     max_common_nums = max(common_nums)
+        #     difference = number - max_common_nums
+        #     print(difference)
             #cycle through the difference and add to the balance until sum to number // I'll need a container that's comparing to the given number
            
 
     
-    return sorted(vals_to_extract, reverse=True)
+    # return sorted(vals_to_extract, reverse=True)
 
-numbers_to_extract = int_vals_to_extract(roman_numeral_values)
 
-print(numbers_to_extract)
+
+
 
 
 
