8853a491f1dbe85486eeff92ab81497d00f8c0bd
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-10 07:38:03 -0400

    Algorithm works

--- strings.py
+++ strings.py
@@ -1,25 +1,33 @@
 
-str1 = 'aab'
-str2 = 'ab'
+str1 = 'a'
+str2 = 'ba'
 
 def find_diff(str1, str2):
     if str1 is None or str2 is None:
         raise TypeError ("You can't have an empty string")
 
-    str1_len = len(str1)
-    str2_len = len(str2)
 
-    if str1_len == str2_len:
-        for i, letter in enumerate(str1):
-            if str1[i] != str2[i]:
-                return letter
-    
 
-# print(find_diff(str1, str2))
 
+
+
+        
 if len(str1) > len(str2):
     compare_string = str1[:len(str2)]
     for i, letter in enumerate(compare_string):
         if compare_string[i] != str2[i]:
             print(letter)
+        
+
+elif len(str1) < len(str2):
+    compare_string = str2[:len(str1)]
+    for i, letter in enumerate(compare_string):
+        if compare_string[i] != str1[i]:
+            print(letter)
+
+else: 
+    for i, letter in enumerate(str1):
+        if compare_string[i] != str2[i]:
+            print(letter)
+        
 
