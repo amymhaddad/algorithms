5fba35755d3dc7cde2938f31d6c3a3774106e07f
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-16 08:16:03 -0400

    Updated algorithm to check if number is a multiple of 5, and to reduce the number by 1 if not

--- roman_numeral/roman_numerals.py
+++ roman_numeral/roman_numerals.py
@@ -3,7 +3,7 @@
 roman_numerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IV", "X"]
 
 
-number = 5
+number = 6
 
 
 roman_numeral_values = {
@@ -23,25 +23,35 @@ for nums in range(1, number +1):
 def conversion(number):
 
     if number == 0:
-        return ""
+        return ''
 
     else:
-        convert = ''
+        convert = []
         reduced_num = number
-        #FIRST see if the number is in the dictionary
-        for arabic_num, letter in roman_numeral_values.items():
-            if number == arabic_num:
-                convert += roman_numeral_values[number]
-                #subtract the converted amount from the given number
-                reduced_num -= arabic_num
 
-        print(reduced_num)
-           
+        if number in multiples:
+            convert.append(roman_numeral_values[number])
+            reduced_num -= number
+        else:
+            reduced_num -= 1
+            convert.append(roman_numeral_values[1])
+       
+        
+        # try:
+        #     #FIRST see if the number is in the dictionary
+        #     for arabic_num, letter in roman_numeral_values.items():
+        #         if number == arabic_num:
+        #             convert.append(roman_numeral_values[number])
+        #             #subtract the converted amount from the given number
+        #             reduced_num -= arabic_num
         # except KeyError:
+            
+
+
         
 
     
-    return convert
+    # return convert + conversion(reduced_num)
     #pass the updated number into conversion
     # return convert and conversion(number-1)
 
