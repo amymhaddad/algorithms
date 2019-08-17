fcfd7c9efa82e517e42a5df7112f0dda6407ce01
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-17 06:51:47 -0400

    Final changes for draft PR review

--- roman_numeral/roman_numerals.py
+++ roman_numeral/roman_numerals.py
@@ -1,34 +1,27 @@
 """Given a roman numeral, find the integer equivalent"""
 
-roman_numerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IV", "X"]
+# roman_numerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IV", "X"]
 
 
 number = 6
 
 
-roman_numeral_values = {
-    1: "I",
-    5: "V",
-    10: "X",
-}
+roman_numeral_values = {1: "I", 5: "V", 10: "X"}
 
-conversion = []
-
-# multiples = []
-# for nums in range(1, number +1):
-#     if nums % 5 == 0:
-#         multiples.append(nums)
 
 def multiples_of_five():
+    """Create a list of numbers that are multiples of 5"""
     multiples = []
-    for nums in range(1, number +1):
+    for nums in range(1, number + 1):
         if nums % 5 == 0:
             multiples.append(nums)
     return multiples
 
+
 def conversion(number):
+    """Convert an Arabic number to its roman numeral equivalent"""
     if number == 0:
-        return ''   
+        return ""
 
     else:
         convert = []
@@ -41,10 +34,7 @@ def conversion(number):
             reduced_num -= 1
             convert.append(roman_numeral_values[1])
 
-
     return "".join(convert) + conversion(reduced_num)
-        
-print(conversion(number))
 
-#Next steps:
-# -Get the converted nums to numerals to get in sorted order
+
+print(conversion(number))
