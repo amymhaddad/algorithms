43de38bbfe33b5ab68be70de818e629366d0d06f
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-19 08:11:36 -0400

    Algorithm works

--- gcd/gcd.py
+++ gcd/gcd.py
@@ -1,5 +1,6 @@
-num1 = 6
-num2 = 4
+num1 = 12
+num2 = 6
+test_num = num2
 #find the factors for each number, then get the max common factor (use set() intersection)
 
 num1_factors = []
@@ -7,5 +8,11 @@ for num_outer in range(1, num1+1):
     for num_inner in range(1, num1):
         if num_outer * num_inner == num1:
             num1_factors.append(num_outer)
-print(num1_factors)
 
+num2_factors = []
+for num_outer in range(1, num2+1):
+    for num_inner in range(1, num2):
+        if num_outer * num_inner == num2:
+            num2_factors.append(num_outer)
+
+print(max(set(num1_factors) & set(num2_factors)))
