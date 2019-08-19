5a6d3dc55e0134071440272e70590425cb06e14a
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-19 08:00:58 -0400

    Algorithm to get factors works

--- gcd/gcd.py
+++ gcd/gcd.py
@@ -3,5 +3,9 @@ num2 = 4
 #find the factors for each number, then get the max common factor (use set() intersection)
 
 num1_factors = []
-for nums in range(1, num1+1):
-    
+for num_outer in range(1, num1+1):
+    for num_inner in range(1, num1):
+        if num_outer * num_inner == num1:
+            num1_factors.append(num_outer)
+print(num1_factors)
+
