83b4f408b8f38d74a67ee29fa8caa95ff3d355fc
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-21 10:22:09 -0400

    Added changes to gcd

--- gcd/gcd.py
+++ gcd/gcd.py
@@ -1,7 +1,5 @@
-num1 = 12
-num2 = 6
-test_num = num2
-#find the factors for each number, then get the max common factor (use set() intersection)
+num1 = 24
+num2 = 1
 
 num1_factors = []
 for num_outer in range(1, num1+1):
@@ -9,19 +7,13 @@ for num_outer in range(1, num1+1):
         if num_outer * num_inner == num1:
             num1_factors.append(num_outer)
 
+import pdb; pdb.set_trace()
 num2_factors = []
 for num_outer in range(1, num2+1):
     for num_inner in range(1, num2):
         if num_outer * num_inner == num2:
             num2_factors.append(num_outer)
 
+
+
 gcd = max(set(num1_factors) & set(num2_factors))
-
-
-def gdc(num1, num2):
-    if num1 % test_num == 0 and num2 % test_num == 0:
-        return test_num
-    else:
-        test_num -= 1
-    
-    
