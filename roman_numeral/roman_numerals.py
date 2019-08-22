04c5b3443c2cd8ffb5b43eca4092754d9d226ecd
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-22 09:34:13 -0400

    Refactored finding the multiples of 5 to 1 line and removed an entire function

--- roman_numeral/roman_numerals.py
+++ roman_numeral/roman_numerals.py
@@ -9,13 +9,13 @@ number = 6
 roman_numeral_values = {1: "I", 5: "V", 10: "X"}
 
 
-def multiples_of_five():
-    """Create a list of numbers that are multiples of 5"""
-    multiples = []
-    for nums in range(1, number + 1):
-        if nums % 5 == 0:
-            multiples.append(nums)
-    return multiples
+# def multiples_of_five():
+#     """Create a list of numbers that are multiples of 5"""
+#     multiples = []
+#     for nums in range(1, number + 1):
+#         if nums % 5 == 0:
+#             multiples.append(nums)
+#     return multiples
 
 
 def conversion(number):
@@ -27,7 +27,7 @@ def conversion(number):
         convert = []
         reduced_num = number
 
-        if number in multiples_of_five():
+        if number % 5 == 0:
             convert.append(roman_numeral_values[number])
             reduced_num -= number
         else:
