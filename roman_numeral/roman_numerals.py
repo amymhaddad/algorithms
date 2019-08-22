d17b2d4f9e6f65f635752cb306267d7202421709
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-22 11:17:26 -0400

    Updates to draft PR

--- roman_numeral/roman_numerals.py
+++ roman_numeral/roman_numerals.py
@@ -3,13 +3,14 @@
 # roman_numerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IV", "X"]
 
 
-number = 6
+number = 15
 
 roman_numeral_values = {1: "I", 5: "V", 10: "X"}
 
+
 def conversion(number):
     """Convert an Arabic number to its roman numeral equivalent"""
-    
+
     if number == 0:
         return ""
 
@@ -17,6 +18,7 @@ def conversion(number):
         convert = []
         reduced_num = number
 
+        # First check: see if number is in dictionary. If so, return it, if not get the largest value from the dictionary
         if number % 5 == 0:
             try:
                 convert.append(roman_numeral_values[number])
@@ -24,24 +26,21 @@ def conversion(number):
             except KeyError:
                 convert.append(roman_numeral_values[10])
                 reduced_num -= 10
-            
+
             return "".join(convert) + conversion(reduced_num)
 
+        # Second check: if the number is larger than 5, then subtract the remainder from the number and extract that result from the dictionary.
+        if number > 5:
+            try:
+                remainder = number % 5
+                multiple_of_five = roman_numeral_values[(number - remainder)]
+                convert.append(multiple_of_five)
+                reduced_num -= number - remainder
+            except KeyError:
+                convert.append(roman_numeral_values[10])
+                reduced_num -= 10
 
-        elif number > 5:
-            if number > 5:
-                try: 
-                    remainder = number % 5
-                    multiple_of_five = roman_numeral_values[(number-remainder)]
-                    convert.append(multiple_of_five)
-                    reduced_num -= (number - remainder)
-                except KeyError:
-                    convert.append(roman_numeral_values[10])
-                    reduced_num -= 10
-
-            
             return "".join(convert) + conversion(reduced_num)
-
 
         elif number < 5:
             reduced_num -= 1
