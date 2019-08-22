820d38e6333446fe4b7de297bbc5eb72fcd5a6d5
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-22 10:51:43 -0400

    Updated roman to account for numbers that are not in dictionary and are larger than 5

--- roman_numeral/roman_numerals.py
+++ roman_numeral/roman_numerals.py
@@ -3,8 +3,7 @@
 # roman_numerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IV", "X"]
 
 
-number = 16
-
+number = 6
 
 roman_numeral_values = {1: "I", 5: "V", 10: "X"}
 
@@ -26,22 +25,22 @@ def conversion(number):
                 convert.append(roman_numeral_values[10])
                 reduced_num -= 10
             
-            #try to get this value from dictionary
-            #except KeyError
-            #Then get the largest value from the dictionary
-            
-            # convert.append(roman_numeral_values[number])
-            # reduced_num -= number
             return "".join(convert) + conversion(reduced_num)
 
 
-        elif number % 5 != 0 and number > 5:
-            import pdb; pdb.set_trace()
-            remainder = number % 5
-            multiple_of_five = roman_numeral_values[(number-remainder)]
-            convert.append(multiple_of_five)
+        elif number > 5:
+            if number > 5:
+                try: 
+                    remainder = number % 5
+                    multiple_of_five = roman_numeral_values[(number-remainder)]
+                    convert.append(multiple_of_five)
+                    reduced_num -= (number - remainder)
+                except KeyError:
+                    convert.append(roman_numeral_values[10])
+                    reduced_num -= 10
 
-            return "".join(convert) + conversion(remainder)
+            
+            return "".join(convert) + conversion(reduced_num)
 
 
         elif number < 5:
