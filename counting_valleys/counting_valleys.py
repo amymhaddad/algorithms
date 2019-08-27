f4e492dd49114e13673989097c34a18771e91634
Amy M Haddad <amy.m.haddad@gmail.com>
2019-08-27 10:37:29 -0600

    Got algorithm to find repeated letters (ie, same step type) to work

--- counting_valleys/counting_valleys.py
+++ counting_valleys/counting_valleys.py
@@ -1,19 +1,48 @@
-s = "UDDDUDUU"
+steps = "DDUUDDUDUUUD"
 
 
-letters = [letter for letter in s]
+# letters = [letter for letter in s]
 
-count_u = 0
-count_d = 0
-for letter in letters:
-    if letter == "U":
-        count_u += 1
-    if letter == "D":
-        count_d += 1
+# count_u = 0
+# count_d = 0
 
-valley_count = 0
+# for letter in letters:
+#     if letter == "U":
+#         count_u += 1
+#     if letter == "D":
+#         count_d += 1
 
-if count_u % count_d == 0:
-    valley_count += 1
 
-print(valley_count)
+# valley_count = 0
+
+# if count_u % count_d == 0:
+#     valley_count += 1
+
+# print(valley_count)
+
+
+
+letter_with_count = []
+
+repeated_letters = ''
+
+for i, letter in enumerate(steps):
+    # if list is empty, then add a letter to it
+    if not repeated_letters:
+        repeated_letters += letter
+    
+    #if letter is in repeated_letters, then we have a repeated letter -- add it to the repeated_letters list
+    elif letter in repeated_letters:
+        repeated_letters += letter
+
+    #letter is not in repeated_letters, then we have a new letter. 
+    #Tally up the current repeated letters and their count and make the new -- unique -- letter the compariosn letter 
+    elif letter not in repeated_letters:
+        if len(repeated_letters) >= 2:
+            letter_with_count.append(repeated_letters)
+        repeated_letters = letter
+
+print(letter_with_count)
+
+# DD UU DD U   D UUU D
+# 2D 2U 2D 1U 1D 3U
